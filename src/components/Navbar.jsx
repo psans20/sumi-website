a1253d04e8e395259481b921fdea604e8e54caa8
TarpCityBitch <travistarpley0@gmail.com>
2023-06-25 16:39:17 -0700

    btn edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,12 +59,12 @@ export default function Navbar () {
 
 function NavButton ({ title }) {
   return (
-    <button className='border-black relative mr-4 border-2 h-[55px] w-60 md:h-[55px] md:w-56 rounded-full shadow-slate-500  shadow-md '>
+    <button className='border-black relative mr-4 border-2 h-[55px]  w-60 md:h-[55px] md:w-56 rounded-full shadow-slate-500 bg-bg2 hover:bg-opacity-80 shadow-md'>
       <div className='absolute h-full w-full rounded-full top-3 top-box  z-20'></div>
       <div className='absolute h-full w-full  top-0 z-10 rounded-full '></div>
       {/* <div className=' rounded-t-3xl bg-gradient-to-b w-full h-1/2 absolute from-white  top-0'></div> */}
       <span
-        className='text-white text-2xl md:text-3xl text-center text-opacity-80  bg2 rounded-full absolute flex h-full w-full items-center justify-center top-0'
+        className='text-white text-2xl md:text-3xl text-center text-opacity-80  rounded-full absolute flex h-full w-full items-center justify-center top-0'
         style={{ fontFamily: 'Pristina' }}
       >
         {title}
